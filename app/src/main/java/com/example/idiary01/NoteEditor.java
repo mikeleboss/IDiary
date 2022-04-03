3fc0faceec60a129c9c404d9622f88101efa13ca
connor <connorchan88@gmail.com>
2022-04-02 22:38:09 -0400

    font can be changed image can be uploaded

--- app/src/main/java/com/example/idiary01/NoteEditor.java
+++ app/src/main/java/com/example/idiary01/NoteEditor.java
@@ -1,33 +1,49 @@
 package com.example.idiary01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.os.BuildCompat;
 import androidx.core.view.inputmethod.EditorInfoCompat;
 import androidx.core.view.inputmethod.InputConnectionCompat;
 import androidx.core.view.inputmethod.InputContentInfoCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Base64;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
+import java.io.ByteArrayOutputStream;
 import java.util.HashSet;
 
 public class NoteEditor extends AppCompatActivity {
 
 
-    String entryID, noteID;
+    String entryID, noteID, imageURI;
     EditText editTextEntryText;
-    Button returner, save, editColor, editFont;
-    int color;
+    Button returner, save, editColor, editFont, picture;
+    int color, font;
+    ImageView image;
+    private static int RESULT_LOAD_IMAGE = 3;
+    Bitmap bm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +55,22 @@ public class NoteEditor extends AppCompatActivity {
         save = (Button) findViewById(R.id.button20);
         editColor = (Button) findViewById(R.id.button26);
         editFont = (Button) findViewById(R.id.button27);
+        picture = (Button) findViewById(R.id.uploadImage);
+        image = (ImageView) findViewById(R.id.imageToUpload);
+
         editTextEntryText = (EditText) findViewById(R.id.editTextEntryText);
+
+
 
         Intent intent = getIntent();
         entryID = intent.getStringExtra("entryID");
         noteID = intent.getStringExtra("noteID");
         String colorID = entryID + ".color";
+        String picID = entryID + ".picture";
+        String fontID = entryID +".font";
+        font = settings.getInt(fontID, 3);
         color = settings.getInt(colorID, 7);
+        imageURI = settings.getString(picID, null);
         editTextEntryText.setText(settings.getString(entryID, ""));
 
         if(color == 1) {
@@ -77,6 +102,37 @@ public class NoteEditor extends AppCompatActivity {
 
         }
 
+        if(font == 1) {
+            editTextEntryText.setTypeface(Typeface.DEFAULT_BOLD);
+
+        }
+        else if(font == 2) {
+            editTextEntryText.setTypeface(Typeface.SERIF);
+
+        }
+        else if(font == 3) {
+            editTextEntryText.setTypeface(Typeface.DEFAULT);
+
+        }
+
+
+        image.setImageBitmap(StringToBitMap(imageURI));
+
+        picture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+
+
+            }
+        });
+
         returner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,6 +144,7 @@ public class NoteEditor extends AppCompatActivity {
             }
         });
 
+
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -95,6 +152,8 @@ public class NoteEditor extends AppCompatActivity {
                 SharedPreferences.Editor editor = settings.edit();
                 editor.putString(entryID, entry);
                 editor.putInt(colorID, color);
+                editor.putInt(fontID, font);
+                editor.putString(picID, BitMapToString(bm));
                 editor.apply();
             }
         });
@@ -145,11 +204,62 @@ public class NoteEditor extends AppCompatActivity {
         editFont.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editTextEntryText.setTypeface(Typeface.SANS_SERIF);
+                if(font < 3){
+                    font++;
+                }
+                else{
+                    font = 1;
+                }
+
+                if(font == 1) {
+                    editTextEntryText.setTypeface(Typeface.DEFAULT_BOLD);
+
+                }
+                else if(font == 2) {
+                    editTextEntryText.setTypeface(Typeface.SERIF);
+
+                }
+                else if(font == 3) {
+                    editTextEntryText.setTypeface(Typeface.DEFAULT);
+
+                }
             }
         });
 
 
 
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imageToUpload);
+            imageView.setImageURI(selectedImage);
+            bm =((BitmapDrawable)imageView.getDrawable()).getBitmap();
+
+        }
+    }
+
+    public String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+
+    public Bitmap StringToBitMap(String encodedString){
+        try {
+            byte [] encodeByte=Base64.decode(encodedString,Base64.DEFAULT);
+            Bitmap bitmap=BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        } catch(Exception e) {
+            e.getMessage();
+            return null;
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/idiary01/entryView.java
+++ app/src/main/java/com/example/idiary01/entryView.java
@@ -1,22 +1,20 @@
 package com.example.idiary01;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatCheckedTextView;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.HashSet;
